84ecc0492721dc1e172fe6c4825e274c05bd6d24
mmJava <maldojavadev@gmail.com>
2017-04-29 16:43:39 -0600

    add exercise 5 creating variables

---
+++ CreatingVariables.java
@@ -0,0 +1,40 @@
+public class CreatingVariables
+{
+	public static void main(String[] args)
+	{
+		int x, y, pinNumber, age;
+		double seconds, e, pi, checking;
+		String firstName, lastName, title, city, state;
+		
+		x = 10;
+		y = 400;
+		age = 39;
+		pinNumber = 123;
+		seconds = 4.71;
+		e = 2.7182818228459045223536;
+		pi = 3.14;
+		checking = 1.89;
+		
+		firstName = "Michael";
+		lastName = "Maldonado";
+		title = "Mr.";
+		city = "Mascoutah";
+		state = "IL";
+		
+		
+		System.out.println("The variable x contains " + x);
+		System.out.println("The value  " + y + " is stored in the variable y.");
+		System.out.println("The experiment took " + seconds +  " seconds.");
+		System.out.println("A favorite irrational # is Euler's number: " + e);
+		System.out.println("Hopefully you have more than $" + checking + "!");
+		System.out.println("My name's " + title + " " + firstName + lastName);
+		System.out.println("I live in " + city + " " + state);
+		System.out.println("PI is : " + pi);
+		System.out.println("my bank pin number is: " + pinNumber);
+		
+				
+	
+	}
+
+
+}
