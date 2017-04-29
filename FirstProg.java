7bdc4988129271279168253eaab9e42063837267
mmJava <maldojavadev@gmail.com>
2017-04-28 20:59:37 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,9 @@
+public class FirstProg
+{
+  public static void main(String[] args)
+  {
+    System.out.println("I, Maldonado Michael, am determined to learn how to code.");
+    System.out.println("Today's date is Tuesday, April 25, 2017.");
+  }
+
+}
